8553e245bfb0dcb47a6cbb6c0d5bba443d27e79a
tymancjo <tomasz.tomanek@gmail.com>
2017-05-12 14:04:36 +0200

    podsumowanie spotkania tk  inter 02, lineup kreator 0000001alfa

--- 04_tkinter_02_appbuild/app_engine.py
+++ 04_tkinter_02_appbuild/app_engine.py
@@ -3,7 +3,7 @@
 # some imports
 import tkinter as tk
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from PIL import ImageTk, Image
 
 # defining some classes here
@@ -15,6 +15,8 @@ class mainWindow:
         # Setting up some object variables
         self.master = master
         self.lineup = lineup
+
+        self.naszeX = 0
 
         # starting work with tkinter interface
         # setting up first frame element with 5pixels distances around
@@ -32,10 +34,10 @@ class mainWindow:
         self.frame_mid = tk.Frame(self.master)
         self.frame_mid.pack(padx=5, pady=5)
 
-        # so we add a canvas element here
-        self.canvas = tk.Canvas(self.frame_mid, width=800, height=250,
-                                background='white')
-        self.canvas.grid(row=0, column=0)
+        # # so we add a canvas element here
+        # self.canvas = tk.Canvas(self.frame_mid, width=500, height=250,
+        #                         background='white')
+        # self.canvas.grid(row=0, column=0)
 
         # bottom frmae to keep controls under canvas
         self.frame_btm = tk.Frame(self.master)
@@ -45,33 +47,31 @@ class mainWindow:
                                    command=self.onClick)
         self.addButton.grid(row=0, column=0, columnspan=10)
 
-    def addImageToCanvas(self, image_file, x=0, y=0):
+    def addImageToCanvas(self, c, image_file, x=0, y=0):
         '''This procedure prints out image file to the window canvas'''
         # Load the image file
         im = Image.open(image_file)
-        # Let's resize it to fit our canvas size
-        canvasX = self.canvas.winfo_width()
-        canvasY = self.canvas.winfo_height()
-
-        imageX, imageY = im.size
-
-        if imageY > canvasY:
-            imageX = int(imageX * (canvasY / imageY))
-            imageY = int(canvasY)
-
-        size = imageX, imageY
-
-
-        im = im.resize(size, Image.ANTIALIAS)
-        # Put the image into a canvas compatible class, and stick in an
-        # arbitrary variable to the garbage collector doesn't destroy it
-        self.canvas.image = ImageTk.PhotoImage(im)
+        im = im.resize((100,100))
+        # # Let's resize it to fit our canvas size
+        # to the garbage collector doesn't destroy it
+        c.image = ImageTk.PhotoImage(im)
         # Add the image to the canvas, and set the anchor to the top left / north west corner
-        self.canvas.create_image(0, 0, image=self.canvas.image, anchor='nw')
+        c.create_image(x, y, image=c.image, anchor='nw')
+        print('Adding image na: {} {}'.format(x, y))
 
     def onClick(self):
-        self.addImageToCanvas(image_file='/home/tomasz/Dropbox/Git/WhoPy/04_tkinter_02_appbuild/img/entelleon.png')
-        print('Adding image')
+
+        self.canvas = []
+        for szafa in self.lineup:
+            # so we add a canvas element here
+            self.canvas.append(tk.Canvas(self.frame_mid, width=100, height=250,
+                                    background='white'))
+
+            self.canvas[-1].grid(row=0, column=self.naszeX)
+
+            self.addImageToCanvas(c=self.canvas[-1], image_file='img/entelleon.png')
+            self.naszeX += 100
+
 
 # Here is where the main app starts
 if __name__ == '__main__':
@@ -80,7 +80,12 @@ if __name__ == '__main__':
     # Defining the main tkinter master window object (parent to all windows)
     root = tk.Tk()
     root.title('Lineup Creator')
-    mW = mainWindow(root)
+    mW = mainWindow(root, [1])
 
+    # root2 = tk.Tk()
+    # root2.title('Drugi')
+    # mW2 = mainWindow(root2)
+
+    # print(mW2.lineup)
     # looping the script to wait till the GIU is closed by user
     root.mainloop()
